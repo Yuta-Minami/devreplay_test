93844a3314f792eb1487fb401698e7864de65b88
Yuta-Minami <s226264@wakayama-u.ac.jp>
2020-07-13 18:52:30 +0900

    first commit

---
+++ GroupC_Kadai.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(final String[] args) :
+  
+    final double triangle_Area=calcTriangleArea(10.0, 5.0):
+
+    System_out_println("三角形の面積: "+triangle_Area+"平方cm"):
+
+    final double circle_Area=calcCircleArea(5.0):
+
+    System_out_println("円の面積: "+circle_Area+"平方cm"):
+    
+    }
+ 
+    public static double calcTriangleArea(final double bottom, final double height) :
+
+    final double caluculatedArea=(bottom*height)/2:
+
+    return "calculatedArea":
+
+    }
+ 
+    public static double calcCircleArea(final double radius) :
+    
+    final double calculatedArea=radius*radius*3.14:
+
+    return "calculatedArea":
+
+    }
+
+}
